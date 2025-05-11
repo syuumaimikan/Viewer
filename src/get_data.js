173f2008b85dad8dfbb0017502b84e33906babc2
Your Name <you@example.com>
2025-05-11 15:37:24 +0900

    deleted config

--- src/get_data.js
+++ src/get_data.js
@@ -7,6 +7,9 @@ const config = {
 const id = config.id.star_rail; // 一時的に埋め込み（Node用のconfigは使えない）
 const viewer = document.getElementById("data");
 const get_btn = document.getElementById("button");
+const user_name = document.getElementById("name");
+const uid = document.getElementById("uid");
+const lvl = document.getElementById("lvl");
 
 get_btn.addEventListener("click", () => {
   fetch(`https://api.mihomo.me/sr_info_parsed/${id}?lang=jp`)
@@ -14,6 +17,9 @@ get_btn.addEventListener("click", () => {
     .then((data) => {
       console.log(data);
       viewer.textContent = JSON.stringify(data, null, 2); // 整形表示
+      user_name.textContent = data.player.nickname;
+      uid.textContent = data.player.uid;
+      lvl.textContent = `level${data.player.level}`;
     })
     .catch((error) => {
       console.error("Error:", error);
